d347db8ffa8b630847b1364c891fb6b7a441c567
hua <halecone@gmail.com>
2020-04-09 17:40:11 +0800

    7.粉丝关注

--- back-end/madblog.py
+++ back-end/madblog.py
@@ -1,9 +1,7 @@
-
 from app import create_app
 from app.extensions import db
 from app.models import User, Post
 from config import Config
-
 
 app = create_app(Config)
 
@@ -12,9 +10,7 @@ app = create_app(Config)
 def hello_world():
     return 'Hello, World!'
 
+
 @app.shell_context_processor
 def make_shell_context():
-    return {
-        'db': db,
-        'User': User,
-        'Post': Post}
+    return {'db': db, 'User': User, 'Post': Post}
